f7e4a10a16788b7c5cdddd133a8170a1ffec054c
Henrique Bontempo <me@hbontempo.dev>
2020-09-08 00:06:56 -0300

    Added test to api/models

--- api/models/book_status_test.go
+++ api/models/book_status_test.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBookStatusModel_TableName(t *testing.T) {
+	t.Run("TableName method", func(t *testing.T) {
+		expectedTableName := "BookStatus"
+		bookStatusModel := BookStatusModel{}
+		assert.Equal(t, expectedTableName, bookStatusModel.TableName(), "Unexpected TableName method return")
+	})
+}
+
+func TestBookModel_String(t *testing.T) {
+	t.Run("String method", func(t *testing.T) {
+		expectedTableName := "{ID:1, Enumerator:a}"
+		bookStatusModel := BookStatusModel{ID: 1, Enumerator: "a"}
+		assert.Equal(t, expectedTableName, bookStatusModel.String(), "Unexpected String method return")
+	})
+}
