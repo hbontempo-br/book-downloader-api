108c147ddb0e39f99e2449f834fe27deb4a51079
Henrique Bontempo <me@hbontempo.dev>
2020-09-08 00:39:44 -0300

    Added tests to api/DTOs

--- api/DTOs/book_link_test.go
+++ api/DTOs/book_link_test.go
@@ -1,1 +1,20 @@
 package DTOs
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"testing"
+)
+
+func TestNewBookLinkDTO(t *testing.T) {
+	t.Run("BookLinkDTO generation", func(t *testing.T) {
+		testRawUrl := "https://test.com"
+		testUrl, err := url.Parse(testRawUrl)
+		if err != nil {
+			t.Error("Test failed on setup, unable to create test URL")
+		}
+		expectedLinkDTO := BookLinkDTO{DownloadLink: testRawUrl}
+		bookLinkDTO := NewBookLinkDTO(*testUrl)
+		assert.Equal(t, expectedLinkDTO, bookLinkDTO, "Unexpected BookLinkDTO")
+	})
+}
--- api/DTOs/book_test.go
+++ api/DTOs/book_test.go
@@ -1,1 +1,40 @@
 package DTOs
+
+import (
+	"github.com/gofrs/uuid"
+	"github.com/hbontempo-br/book-downloader-api/api/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNewBookDTO(t *testing.T) {
+	t.Run("BookDTO generation", func(t *testing.T) {
+
+		testUUID, err := uuid.NewV4()
+		if err != nil {
+			t.Error("Test failed on setup, unable to create random UUID")
+		}
+		testTime := time.Now()
+		bookModel := models.BookModel{
+			ID:        1,
+			BookKey:   testUUID,
+			Name:      "name",
+			Mask:      "mask",
+			StatusID:  1,
+			Status:    models.BookStatusModel{1, "status"},
+			CreatedAt: &testTime,
+			DeletedAt: &testTime,
+		}
+		bookDTO := NewBookDTO(bookModel)
+		expectedBookDTO := BookDTO{
+			Name:    "name",
+			Mask:    "mask",
+			BookKey: testUUID.String(),
+			Status:  "status",
+		}
+
+		assert.Equal(t, expectedBookDTO, bookDTO, "Unexpected BookDTO")
+
+	})
+}
